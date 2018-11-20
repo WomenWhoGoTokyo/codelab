64ba30974931c0124fbe36b3b6073098cd4801a7
mi-bear <kumajet@gmail.com>
2018-11-20 19:32:32 +0900

    Add 表示されている文字を変えてみよう！の元となるデータを追加

--- guestbook/index.go
+++ guestbook/index.go
@@ -10,6 +10,17 @@ import (
 
 var indexTmpl = template.Must(template.ParseFiles("./view/index.html"))
 
+var title = "ゲストブック"
+var description = "結婚式などの受付で名前や住所, メッセージを記帳してもらうためのノートのことです。"
+
+// IndexTemplate is a structure of index template.
+type IndexTemplate struct {
+	Title       string
+	Description string
+	Count       int
+	Messages    []*Message
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	ctx, err := appengine.Namespace(c, r.Host)
@@ -25,7 +36,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := indexTmpl.Execute(w, msgs); err != nil {
+	idxt := &IndexTemplate{
+		Title:       title,
+		Description: description,
+		Count:       len(msgs),
+		Messages:    msgs,
+	}
+
+	if err := indexTmpl.Execute(w, idxt); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
