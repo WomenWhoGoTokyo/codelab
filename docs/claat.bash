10bde51de4b3c0333f90ad93e90129749f22140a
Michiru Shirakawa <kumajet@gmail.com>
2018-11-20 20:55:21 +0900

    Add index (#3)
    * Add indexページのリンクを追加

--- docs/claat.bash
+++ docs/claat.bash
@@ -3,6 +3,7 @@
 declare -a codelabs;
 codelabs=(
     "gocon-2018-autumn-setup" "1nBB9Sl-mnKE5htc_Fbx_-Q3VUY1YIfF_C3Wi_HFgzoo"
+    "google-app-engine-go"    "1AjOewYLEtWy-x8RGQtxQe1vMCTKia15CK3iKczmfoQM"
 )
 
 for ((i = 0; i < ${#codelabs[@]}; i+=2)) {
