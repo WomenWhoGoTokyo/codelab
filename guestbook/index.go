86e22468ac22e07ba149f168ecd4e511dc170154
mi-bear <kumajet@gmail.com>
2018-11-22 17:13:03 +0900

    Modified コードラボで編集している分を反映

--- guestbook/index.go
+++ guestbook/index.go
@@ -10,8 +10,8 @@ import (
 
 var indexTmpl = template.Must(template.ParseFiles("./view/index.html"))
 
-var title = "ゲストブック"
-var description = "結婚式などの受付で名前や住所, メッセージを記帳してもらうためのノートのことです。"
+var title = "Wedding Guest Book"
+var description = "Gopher & Gopher 2018.11.25"
 
 // IndexTemplate is a structure of index template.
 type IndexTemplate struct {
@@ -30,7 +30,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var msgs []*Message
-	q := datastore.NewQuery("Message").Order("-createdAt").Limit(10)
+	q := datastore.NewQuery("Message").Order("-createdAt").Limit(15)
 	if _, err := q.GetAll(ctx, &msgs); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
