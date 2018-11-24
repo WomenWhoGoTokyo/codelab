77fc68a54d3a7cd95d0c1aebe3b7af100f29128a
Michiru Shirakawa <kumajet@gmail.com>
2018-11-24 19:00:42 +0900

    Modified keyの名前をIDに変更 (#8)

--- guestbook/message.go
+++ guestbook/message.go
@@ -4,7 +4,7 @@ import "time"
 
 // Message is a structure of messages to be posted to guest book.
 type Message struct {
-	KeyID     int64     `datastore:"-"`
+	ID        int64     `datastore:"-"`
 	Name      string    `datastore:"name"`
 	Message   string    `datastore:"message"`
 	CreatedAt time.Time `datastore:"createdAt"`
