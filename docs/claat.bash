673ea099ec20a7d6e7cdb018ec021e2425105680
Michiru Shirakawa <kumajet@gmail.com>
2019-05-13 22:25:03 +0900

    Go Conference 2019 Spring のコードラボ Docs (#14)
    * Modified contents of cloud functions go contents

--- docs/claat.bash
+++ docs/claat.bash
@@ -2,9 +2,11 @@
 
 declare -a codelabs;
 codelabs=(
-    "gocon-2018-autumn-setup" "1nBB9Sl-mnKE5htc_Fbx_-Q3VUY1YIfF_C3Wi_HFgzoo"
-    "google-cloud-shell-go"   "1tmQBU8WtUIQ_Yb2STllFCa3lE8DQsMFnkikvcjMjF1c"
-    "google-app-engine-go"    "1AjOewYLEtWy-x8RGQtxQe1vMCTKia15CK3iKczmfoQM"
+    "gocon-2018-autumn-setup"   "1nBB9Sl-mnKE5htc_Fbx_-Q3VUY1YIfF_C3Wi_HFgzoo"
+    "gocon-2019-spring-setup"   "1SBE8eGYPdnZVuMffOA0t4OT_6sxndTjNqmEOv-d61mA"
+    "google-cloud-shell-go"     "1tmQBU8WtUIQ_Yb2STllFCa3lE8DQsMFnkikvcjMjF1c"
+    "google-app-engine-go"      "1AjOewYLEtWy-x8RGQtxQe1vMCTKia15CK3iKczmfoQM"
+    "google-cloud-functions-go" "1uwGLNnjLfFky0mvQuAKu7CIVTt6jO2rIMD4AkxrRUj4"
 )
 
 for ((i = 0; i < ${#codelabs[@]}; i+=2)) {
