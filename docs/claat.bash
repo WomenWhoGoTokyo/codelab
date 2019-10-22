94f38bc1eccd1bae2c3937f0e56f4d6a8c1d261e
mom0tomo <momo76w@gmail.com>
2019-10-22 18:58:46 +0900

    update claat.bash

--- docs/claat.bash
+++ docs/claat.bash
@@ -4,6 +4,7 @@ declare -a codelabs;
 codelabs=(
     "gocon-2018-autumn-setup"   "1nBB9Sl-mnKE5htc_Fbx_-Q3VUY1YIfF_C3Wi_HFgzoo"
     "gocon-2019-spring-setup"   "1SBE8eGYPdnZVuMffOA0t4OT_6sxndTjNqmEOv-d61mA"
+    "gocon-2019-autumn-setup"   "1y9GR6RntgL7R-Ll3R_LGVy-c_lsibk5-txsbu2R5xQI"
     "google-cloud-shell-go"     "1tmQBU8WtUIQ_Yb2STllFCa3lE8DQsMFnkikvcjMjF1c"
     "google-app-engine-go"      "1AjOewYLEtWy-x8RGQtxQe1vMCTKia15CK3iKczmfoQM"
     "google-cloud-functions-go" "1uwGLNnjLfFky0mvQuAKu7CIVTt6jO2rIMD4AkxrRUj4"
