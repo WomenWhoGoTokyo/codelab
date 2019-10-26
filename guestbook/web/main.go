cad1943f6782859420fa3d4c72cd7f70e5a87fe2
mom0tomo <momo76w@gmail.com>
2019-10-26 16:56:04 +0900

    remove log printings

--- guestbook/web/main.go
+++ guestbook/web/main.go
@@ -18,6 +18,4 @@ func main() {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
-	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
