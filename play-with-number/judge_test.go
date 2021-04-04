25992e633fb8c10708b6f05e5c3b6cd3d8cc186e
quwen <s30gd.3@gmail.com>
2021-04-04 15:40:12 +0900

    Change args from struct to int.

--- play-with-number/judge_test.go
+++ play-with-number/judge_test.go
@@ -3,32 +3,28 @@ package main
 import "testing"
 
 func Test_PrimeNumdeterminer(t *testing.T) {
-	type args struct {
-		num int
-	}
+
 	tests := []struct {
 		name string
-		args args
+		args int
 		want string
 	}{
 		{
 			name: "return '素数です' if you set '5'",
-			args: args{
-				5,
-			},
+			args: 5,
+
 			want: "素数です",
 		},
 		{
 			name: "return '素数ではありません' if you set '6'",
-			args: args{
-				6,
-			},
+			args: 6,
+
 			want: "素数ではありません",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotResult := PrimeNumdeterminer(tt.args.num); gotResult != tt.want {
+			if gotResult := PrimeNumdeterminer(tt.args); gotResult != tt.want {
 				t.Errorf("PrimeNumdeterminer() = %v, want %v", gotResult, tt.want)
 			}
 		})
