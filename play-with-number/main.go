ddac4ec647e5d7140fd5b4dfed74995428c5f37e
quwen <s30gd.3@gmail.com>
2021-04-04 09:46:39 +0900

    Add option

--- play-with-number/main.go
+++ play-with-number/main.go
@@ -1,14 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var game string
 
 func main() {
 
-	fmt.Print("数字を入力してください\n")
+	flag.Parse()
 
-	var num int
-	fmt.Scan(&num)
+	// TODO:timeを使って今日の日付を数字に変換する
 
-	fmt.Printf("この数字は%s\n", PrimeNumdeterminer(num))
+	if game != "" {
+		fmt.Print("数字を入力してください\n")
 
+		var num int
+		fmt.Scan(&num)
+
+		fmt.Printf("この数字は%s\n", PrimeNumdeterminer(num))
+	} else {
+		fmt.Printf("オプションを指定してください")
+	}
+}
+
+func init() {
+
+	flag.StringVar(&game, "game", "", "ゲームのメニューを選択")
 }
