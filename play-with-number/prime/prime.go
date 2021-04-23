85374b2034c3aac6d93ecea720b0a72d3288007e
mom0tomo <momo76w@gmail.com>
2021-04-24 08:26:45 +0900

    n==1の場合の処理を修正

--- play-with-number/prime/prime.go
+++ play-with-number/prime/prime.go
@@ -8,8 +8,8 @@ func IsPrime(n int) bool {
 	squareroot := math.Sqrt(float64(n))
 	result := true
 
+	//TODO: コマンドラインから取得した値が1の場合の処理を書く
 	for i := 2; i <= int(squareroot); i++ {
-		//TODO: コマンドラインから取得した値が1の場合の条件分岐処理を書く
 		if n%i == 0 {
 			//TODO: 結果の判定をする
 			break
